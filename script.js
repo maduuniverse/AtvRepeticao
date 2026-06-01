1fc3b388efd41492a938843330096823ecc6ec00
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-01 08:39:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let numero = Number(prompt("Digite um número maior que 100: "));
+while (numero <= 100) {
+    alert("Número inválido. Digite um número maior que 100.");
+    numero = Number(prompt("Digite um número maior que 100: "));
+}
