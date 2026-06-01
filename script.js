cf0c5e6272efb65275c3c5705cd6c20b3320f3ee
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-01 09:28:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let numero = Number(prompt("Digite um número para iniciar a contagem decrescente: "));
-while (numero >= 0) {
-    alert(numero);
-    numero--;
+let contador = 0
+let numero = Number(prompt("Digite um número (0 para sair): "))
+while (numero !== 0) {
+    if (numero > 0) {
+        contador++
+    }
+    numero = Number(prompt("Digite outro número (0 para sair): "))
 }
+alert(`Quantidade de números positivos digitados: ${contador}`)
